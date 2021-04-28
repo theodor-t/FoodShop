77179071e81eca018f35c74f9e39f7941160637c
theodorerlax <sampjohn@mail.ru>
2021-04-28 19:03:59 +0300

    quantity to do

--- js/shop/cart/Cart.js
+++ js/shop/cart/Cart.js
@@ -8,6 +8,7 @@ class Cart {
         StyleLoader.renderCart(this._cartItems);
         this.updateCountPrice();
         this.productRemoveHandler();
+        this.quantityHandler();
     }
 
     updateCountPrice() {
@@ -38,20 +39,46 @@ class Cart {
     }
 
     productRemove(event) {
-        const parent = this.getParentElement(event.target, "row mb-4");
+        const parent = this.getTargetElement(event.target, "row mb-4");
         this._cartItems.splice(
-            this._cartItems.findIndex(({id}) => Number(parent.getAttribute("data-id")) === id), 1
+            this._cartItems.findIndex(({id}) => Number(parent.getAttribute("data-id")) === id)
+            , 1
         );
         localStorage.setItem("cart", JSON.stringify(this._cartItems));
         this.updateCountPrice();
         parent?.remove();
     }
 
-    getParentElement(elem, selector) {
+    getTargetElement(elem, selector) {
         for (; elem && elem !== document; elem = elem.parentElement) {
             if (elem.classList.value === selector) return elem
         }
         return null;
     }
+
+    quantityHandler() {
+        const increaseQuantityBtns = document.querySelectorAll(".increase__item");
+        const decreaseQuantityBtns = document.querySelectorAll(".decrease__item");
+
+        increaseQuantityBtns.forEach(btn => {
+            btn.addEventListener("click", (evt) => {
+                this.modifyQuantity(evt, 1)
+            })
+        });
+
+        decreaseQuantityBtns.forEach(btn => {
+            btn.addEventListener("click", (evt) => {
+                this.modifyQuantity(evt, -1)
+            })
+        })
+    }
+
+    modifyQuantity(evt, value) {
+        const quantityHolder = this.getTargetElement(
+            evt.target, "def-number-input number-input safari_only mb-0 w-100")
+            .querySelector(".quantity");
+        quantityHolder.value += value;
+    }
+
 
 }
